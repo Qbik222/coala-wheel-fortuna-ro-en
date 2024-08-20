ada448451a90a31b9dc488614333859f3aa7cefa
yurii <ignatovyw@gmail.com>
2024-08-21 00:37:34 +0300

    code clean

--- front/js/main.js
+++ front/js/main.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () =>{
     const popupDays = document.querySelectorAll(".popup__days-item");
     const popupDaysMob = document.querySelectorAll(".days__item");
     let currentDay = 5
-
     function setDays(days, currentDay){
         days.forEach((day, i) =>{
             ++i
@@ -12,7 +11,6 @@ document.addEventListener("DOMContentLoaded", () =>{
             day.classList.toggle("active", i === currentDay);
         })
     }
-
     function daysRemind(days, classAnim) {
         let delay = 900;
         days.forEach((day, i) => {
@@ -22,69 +20,35 @@ document.addEventListener("DOMContentLoaded", () =>{
             }, delay * i);
         });
     }
-
-    // const randomInterval = Math.random() * (20000 - 10000) + 10000;
-    const randomInterval = Math.random() * (6000 - 4000) + 4000;
-
-
-
+    // const randomInterval = Math.random() * (6000 - 4000) + 4000;
     function addFireworks(containerSelector, numberOfFireworks) {
-        // Створення нового блоку .fireworks-wrap
         const fireworksWrap = document.createElement('div');
         fireworksWrap.className = 'fireworks-wrap';
-
-        // Додавання вказаної кількості .firework елементів до fireworksWrap
         for (let i = 0; i < numberOfFireworks; i++) {
             const firework = document.createElement('div');
             firework.className = 'firework';
             fireworksWrap.appendChild(firework);
         }
-
-        // Знаходимо контейнер, в який хочемо додати fireworksWrap
         const container = document.querySelector(containerSelector);
-
-        // Додаємо fireworksWrap до контейнера
         if (container) {
             container.appendChild(fireworksWrap);
         } else {
             console.error(`Контейнер з селектором "${containerSelector}" не знайдено.`);
         }
     }
-
     function removeFireworks(containerSelector) {
-        // Знаходимо контейнер
         const container = document.querySelector(containerSelector);
-
-        // Якщо контейнер існує, знаходимо в ньому блок .fireworks-wrap і видаляємо його
         if (container) {
             const fireworksWrap = container.querySelector('.fireworks-wrap');
             if (fireworksWrap) {
                 fireworksWrap.remove();
             } else {
-                console.warn(`Елемент з класом ".fireworks-wrap" не знайдено в контейнері ${containerSelector}.`);
+                console.error(`Елемент з класом ".fireworks-wrap" не знайдено в контейнері ${containerSelector}.`);
             }
         } else {
             console.error(`Контейнер з селектором "${containerSelector}" не знайдено.`);
         }
     }
-
-// Використання функції для видалення блоку
-//     removeFireworks('.container');
-
-
-
-
-// Використання функції для додавання блоку
-//
-
-
-
-
-
-
-
-
-
     function startRandomInterval() {
         const randomInterval = Math.random() * (20000 - 10000) + 10000; // Випадковий інтервал між 10 і 20 секундами
         daysRemind(days, "remind");
@@ -92,14 +56,11 @@ document.addEventListener("DOMContentLoaded", () =>{
         daysRemind(popupDaysMob, "remind");
         setTimeout(startRandomInterval, randomInterval);
     }
-
     startRandomInterval();
     daysRemind(days, "remind")
-
     setDays(days, currentDay)
     setDays(popupDays, currentDay)
     setDays(popupDaysMob, currentDay)
-// addAnimationClass()
 
 /// wheel logic
     const wheelSections = document.querySelector(".wheel__sections")
@@ -120,9 +81,7 @@ document.addEventListener("DOMContentLoaded", () =>{
     bubleBtn.addEventListener("mouseout", (e) =>{
         wheelBuble.classList.add("_hidden")
     })
-
     document.addEventListener("click", e => e.target === bubleBtn ? null : wheelBuble.classList.add("_hidden"))
-
     let prizes = ['iphone', 'ecoflow', 'merch', 'nothing', "bonuses"]
     function getRandomPrize(arr) {
         return arr[Math.floor(Math.random() * prizes.length)];
@@ -144,13 +103,9 @@ document.addEventListener("DOMContentLoaded", () =>{
         const popupRightArrow = document.querySelectorAll(".popup__decor-right")
         currentDay === 7 ? popupBody.classList.add("_done") : popup.classList.add("_incomplete")
         document.querySelector(".fav-page").classList.remove("bgScale")
-
-
-
         function addAnim(arr, classAnim){
             arr.forEach(item => item.classList.add(`${classAnim}`) )
         }
-
         //popup animations
         popupBody.classList.add("popupMainAnim")
         setTimeout(() =>{
@@ -161,10 +116,7 @@ document.addEventListener("DOMContentLoaded", () =>{
         setTimeout( () => popupTitle.forEach(item => item.classList.add("popupTitleAnim")), 1800)
         setTimeout( () => popupLeftArrow.forEach(item => item.classList.add("popupLeftArrAnim")), 2400)
         setTimeout( () => popupRightArrow.forEach(item => item.classList.add("popupRightArrAnim")), 2700)
-
         //popup animations
-
-
         closeBtn.addEventListener("click", () =>{
             popup.classList.contains('_nothing') === true ? null : addFireworks(".wheel", 7)
             wheel.classList.add("_lock")
@@ -174,18 +126,11 @@ document.addEventListener("DOMContentLoaded", () =>{
             popupContainer.classList.remove("_opacity", "_zIndex")
             popup.classList.remove(`${showClass}`, '_done', '_incomplete')
             removeFireworks(".popups");
-            console.log(popup.classList.contains('_nothing'))
-
-            // popupContainer.classList.contains('_nothing') === true ?  null :
-
-
         })
     }
     function spinWheel(position, animation, sections, btn, wheel, arrow, prize, spinBg, salut){
         sections.addEventListener("animationend", () =>{
             sections.style.transform = `translate(-50%, -50%) rotate(${position}deg)`
-            // prize === "nothing" ? null : salut.classList.add("_opacity", "_zIndex")
-            console.log(prize)
         }, {once: true})
         sections.classList.add(`${animation}`)
         arrow.style.opacity = "0"
@@ -194,7 +139,6 @@ document.addEventListener("DOMContentLoaded", () =>{
         spinBg.classList.add("showSpinBg")
         btn.style.pointerEvents = "none"
     }
-
     function firstSpin(sections, btn, wheel, arrow, prize, spinBg, salut){
         btn.addEventListener("click", () =>{
             if(prize === "iphone"){
@@ -219,21 +163,12 @@ document.addEventListener("DOMContentLoaded", () =>{
             }
         })
     }
-
     firstSpin(wheelSections, wheelBtn, wheelWrap, wheelArrow, getRandomPrize(prizes), spinBg, salut)
-
-
-
-
-
-
 //// accordion
-
     const accordionHeaders = document.querySelectorAll('.accordion__header');
     accordionHeaders.forEach(header => {
         header.addEventListener('click', () => {
             const content = header.nextElementSibling;
-
             document.querySelectorAll('.accordion__content').forEach(item => {
                 if (item !== content) {
                     item.style.display = 'none';
@@ -249,63 +184,4 @@ document.addEventListener("DOMContentLoaded", () =>{
             }
         });
     });
-
-
-//// for test
-
-    const btnsWrap = document.querySelector('.btns-wrap');
-
-    const buttons = btnsWrap.querySelectorAll('button');
-
-    const dropIphoneButton = document.querySelector('.drop-iphone');
-    const dropEcoflowButton = document.querySelector('.drop-ecoflow');
-    const dropNothingButton = document.querySelector('.drop-nothing');
-    const dropMerchButton = document.querySelector('.drop-merch');
-    const dropBonusButton = document.querySelector('.drop-bonus');
-    const dropDone = document.querySelector('.drop-done');
-    const dropLock = document.querySelector('.drop-lock');
-    const dropSign = document.querySelector('.drop-sign');
-// const dropIncomplete = document.querySelector('.drop-incomplete');
-    const dropMenu = document.querySelector('.drop-menu');
-
-    dropMenu.addEventListener("click", ()=> btnsWrap.classList.toggle("_hidden"))
-
-    firstSpin(wheelSections, dropIphoneButton, wheelWrap, wheelArrow, "iphone", spinBg, salut)
-    firstSpin(wheelSections, dropEcoflowButton, wheelWrap, wheelArrow, "ecoflow", spinBg, salut)
-    firstSpin(wheelSections, dropNothingButton, wheelWrap, wheelArrow, "nothing", spinBg, salut)
-    firstSpin(wheelSections, dropMerchButton, wheelWrap, wheelArrow, "merch", spinBg, salut)
-    firstSpin(wheelSections, dropBonusButton, wheelWrap, wheelArrow, "bonuses", spinBg, salut)
-
-    buttons.forEach(item =>{
-        item.addEventListener("click", () => {
-            wheelSections.style.animationDuration = "0.1s"
-            wheelSections.addEventListener("animationend", () =>{
-                wheelSections.style.animationDuration = "8s"
-            })
-        })
-    })
-
-
-    dropLock.addEventListener("click", () =>{
-    wheelWrap.classList.toggle("_lock")
-    document.querySelector(".progress").classList.toggle("_lock")
-    wheelWrap.classList.remove("_sign")
-    document.querySelector(".progress").classList.remove("_sign")
-    })
-    dropSign.addEventListener("click", () =>{
-        wheelWrap.classList.toggle("_sign")
-        document.querySelector(".progress").classList.toggle("_sign")
-        wheelWrap.classList.remove("_lock")
-        document.querySelector(".progress").classList.remove("_lock")
-
-    })
-
-    dropDone.addEventListener("click", () => currentDay = 7)
-
 })
-
-
-
-
-
-
